d4a50837b278e6607ea1839d705b5cc0f0a532ab
RuslanZaripov <zaripovruslan864@gmail.com>
2022-11-23 22:01:48 +0300

    code refactored

--- rust/proc-macro-builder/src/lib.rs
+++ rust/proc-macro-builder/src/lib.rs
@@ -1,52 +1,7 @@
 use proc_macro2::{Ident, TokenStream};
 
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed};
-
-fn get_inner_ty<'a>(wrapper: &'a str, field_type: &'a syn::Type) -> Option<&'a syn::Type> {
-    if let syn::Type::Path(syn::TypePath { path, .. }) = field_type {
-        // eprintln!("path: {:#?}", path);
-        if path.segments.len() != 1 {
-            return None;
-        }
-        let segment = path.segments.first().unwrap();
-        if segment.ident != wrapper {
-            return None;
-        }
-        if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
-            args,
-            ..
-        }) = &segment.arguments
-        {
-            if args.len() != 1 {
-                return None;
-            }
-            if let syn::GenericArgument::Type(ty) = &args.first().unwrap() {
-                return Some(ty);
-            }
-        }
-    }
-    None
-}
-
-fn has_builder_attr(field: &Field) -> bool {
-    field.attrs.iter().any(|attr| {
-        let syn::Attribute { path, .. } = attr;
-        return path.segments.len() == 1 && path.segments.first().unwrap().ident == "builder";
-    })
-}
-
-fn get_vec_inner_ty(field_type: &syn::Type) -> Option<&syn::Type> {
-    get_inner_ty("Vec", field_type)
-}
-
-fn get_option_inner_ty(field_type: &syn::Type) -> Option<&syn::Type> {
-    get_inner_ty("Option", field_type)
-}
-
-fn is_option(field_type: &syn::Type) -> bool {
-    get_option_inner_ty(field_type).is_some()
-}
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -63,8 +18,7 @@ pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     {
         named
     } else {
-        // TODO: write better error message, in case Builder derived for enum
-        unimplemented!();
+        panic!("Only structs supported");
     };
 
     let b_fields = struct_fields.iter().map(|field| {
@@ -81,11 +35,14 @@ pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let b_setters = struct_fields.iter().map(|field| {
         let default_setter = generate_default_setter(&field);
         match generate_builder_attr_setters(&field) {
-            Some((true, builder_setter)) => builder_setter,
-            Some((false, builder_setter)) => {
-                quote! {
-                    #default_setter
-                    #builder_setter
+            Some((same_idents, builder_setter)) => {
+                if same_idents {
+                    builder_setter
+                } else {
+                    quote! {
+                        #default_setter
+                        #builder_setter
+                    }
                 }
             }
             None => default_setter,
@@ -95,15 +52,12 @@ pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let build_fields = struct_fields.iter().map(|field| {
         let ident = &field.ident;
         let ty = &field.ty;
-        if is_option(ty) || has_builder_attr(field) {
-            quote! {
-                #ident: self.#ident.clone()
-            }
+        let value = if is_option(ty) || has_builder_attr(field) {
+            quote! { self.#ident.clone() }
         } else {
-            quote! {
-                #ident: self.#ident.take().ok_or(anyhow::anyhow!("{} is not set", stringify!(#ident)))?
-            }
-        }
+            quote! { self.#ident.take().ok_or(anyhow::anyhow!("{} is not set", stringify!(#ident)))? }
+        };
+        quote! { #ident: #value }
     });
 
     let none_fields = struct_fields.iter().map(|field| {
@@ -141,17 +95,63 @@ pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             }
         }
     };
-    // eprintln!("TOKENS:\n {}", tokens);
+
     proc_macro::TokenStream::from(tokens)
+}
+
+fn get_inner_ty<'a>(wrapper: &'a str, field_type: &'a syn::Type) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(syn::TypePath { path, .. }) = field_type {
+        if path.segments.len() != 1 {
+            return None;
+        }
+        let segment = path.segments.first().unwrap();
+        if segment.ident != wrapper {
+            return None;
+        }
+        if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            args,
+            ..
+        }) = &segment.arguments
+        {
+            if args.len() != 1 {
+                return None;
+            }
+            if let syn::GenericArgument::Type(ty) = &args.first().unwrap() {
+                return Some(ty);
+            }
+        }
+    }
+    None
+}
+
+fn has_builder_attr(field: &Field) -> bool {
+    field.attrs.iter().any(|attr| {
+        let syn::Attribute { path, .. } = attr;
+        return path.segments.len() == 1 && path.segments.first().unwrap().ident == "builder";
+    })
+}
+
+fn get_vec_inner_ty(field_type: &syn::Type) -> &syn::Type {
+    match get_inner_ty("Vec", field_type) {
+        Some(inner_ty) => inner_ty,
+        None => panic!("expected Vec"),
+    }
+}
+
+fn get_option_inner_ty_or_else_ty(field_type: &syn::Type) -> &syn::Type {
+    match get_inner_ty("Option", field_type) {
+        Some(inner_ty) => inner_ty,
+        None => field_type,
+    }
+}
+
+fn is_option(field_type: &syn::Type) -> bool {
+    get_inner_ty("Option", field_type).is_some()
 }
 
 fn generate_default_setter(field: &&Field) -> TokenStream {
     let ident = &field.ident;
-    let ty = &field.ty;
-    let ident_ty = match get_option_inner_ty(ty) {
-        Some(inner_ty) => inner_ty,
-        None => ty,
-    };
+    let ident_ty = get_option_inner_ty_or_else_ty(&field.ty);
     let value = if has_builder_attr(field) {
         quote! { #ident }
     } else {
@@ -165,40 +165,41 @@ fn generate_default_setter(field: &&Field) -> TokenStream {
     }
 }
 
+fn make_error_msg<T: ToTokens>(tokens: &T) -> Option<(bool, TokenStream)> {
+    Some((
+        false,
+        syn::Error::new_spanned(tokens, "expected `builder(each = \"...\")`").to_compile_error(),
+    ))
+}
+
 fn generate_builder_attr_setters(field: &Field) -> Option<(bool, TokenStream)> {
     for attr in &field.attrs {
         match attr.parse_meta() {
-            Ok(syn::Meta::List(meta_list)) => {
-                if meta_list.path.segments.len() != 1 {
+            Ok(syn::Meta::List(ref meta_list)) => {
+                let segments = &meta_list.path.segments;
+                if segments.len() != 1 {
                     return None;
                 }
-                if meta_list.path.segments.first().unwrap().ident != "builder" {
+                if segments.first().unwrap().ident != "builder" {
                     return None;
                 }
-                return match meta_list.nested.first() {
-                    Some(syn::NestedMeta::Meta(syn::Meta::NameValue(meta_name_value))) => {
-                        if meta_name_value.path.segments.len() != 1 {
+                let nested = &meta_list.nested;
+                return match nested.first() {
+                    Some(syn::NestedMeta::Meta(syn::Meta::NameValue(ref meta_name_value))) => {
+                        let segments = &meta_name_value.path.segments;
+                        if segments.len() != 1 {
                             return None;
                         }
-                        if meta_name_value.path.segments.first().unwrap().ident != "each" {
-                            return Some((
-                                false,
-                                syn::Error::new_spanned(
-                                    meta_list,
-                                    "expected `builder(each = \"...\")`",
-                                )
-                                .to_compile_error(),
-                            ));
+                        if segments.first().unwrap().ident != "each" {
+                            return make_error_msg(meta_list);
                         }
-                        let ident = &field.ident;
-                        let attr_ident = match &meta_name_value.lit {
+                        let lit = &meta_name_value.lit;
+                        let attr_ident = match lit {
                             syn::Lit::Str(lit_str) => Ident::new(&lit_str.value(), lit_str.span()),
-                            _ => panic!("builder attribute can only be used on Vec fields"),
+                            _ => return make_error_msg(lit),
                         };
-                        let vec_inner_ty = match get_vec_inner_ty(&field.ty) {
-                            Some(inner_ty) => inner_ty,
-                            None => panic!("builder attribute can only be used on Vec fields"),
-                        };
+                        let ident = &field.ident;
+                        let vec_inner_ty = get_vec_inner_ty(&field.ty);
                         Some((
                             ident.as_ref().unwrap() == &attr_ident,
                             quote! {
@@ -209,10 +210,10 @@ fn generate_builder_attr_setters(field: &Field) -> Option<(bool, TokenStream)> {
                             },
                         ))
                     }
-                    _ => None,
+                    _ => make_error_msg(nested),
                 };
             }
-            Ok(_meta) => todo!(),
+            Ok(ref meta) => make_error_msg(meta),
             Err(_) => continue,
         };
     }
