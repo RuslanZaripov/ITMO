38fd69a9f042306ce21aeca362e47f1a8cd77bcc
RuslanZaripov <zaripovruslan864@gmail.com>
2023-06-26 21:40:51 +0300

    Delete unnecessary comments

--- data-analysis/lab-6/boosting.py
+++ data-analysis/lab-6/boosting.py
@@ -10,46 +10,47 @@ def get_bounds(m_values):
     return m_values.min() - 0.5, m_values.max() + 0.5
 
 
-# Load the chips dataset
+# Повторите предыдущие два пункта, но вместо случайного леса используйте какой-нибудь алгоритм бустинга.
+
+# 1. Постройте график зависимости качества классификации от числа деревьев
+# на тестовой и тренировочной части набора данных.
+
 df = pd.read_csv("chips.csv")
 
 features = ['x', 'y']
 
-# Split the data into X (features) and y (target)
 X = df[features]
 y = df['class']
 
-# Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-# Create the AdaBoost classifier
 ada_clf = AdaBoostClassifier(
     base_estimator=DecisionTreeClassifier(max_depth=1),
-    n_estimators=200,
+    n_estimators=100,
     learning_rate=0.5,
     random_state=42
 )
 
-# Train the classifier
-ada_clf.fit(X_train, y_train)
+ada_clf.fit(X_train.values, y_train.values)
 
-# Evaluate the classifier on the training and test sets
-train_scores = list(ada_clf.staged_score(X_train, y_train))
-test_scores = list(ada_clf.staged_score(X_test, y_test))
+train_scores = list(ada_clf.staged_score(X_train.values, y_train.values))
+test_scores = list(ada_clf.staged_score(X_test.values, y_test.values))
 
-# Plot the classification quality versus the number of trees
 plt.plot(train_scores, label="Train")
 plt.plot(test_scores, label="Test")
-plt.xlabel("Trees Number")
+plt.xlabel("Trees amount")
 plt.ylabel("Classification Quality")
 plt.legend()
 plt.show()
 
-fig, axs = plt.subplots(2, 4, figsize=(30, 20))
-axs = axs.flatten()
-ax_index = 0
+# 2. Визуализируйте, как всё пространство классифицируется алгоритмом для разного числа деревьев.
+# Должно получиться несколько картинок.
+# Необязательно строить картинки для каждого шага.
+# Можно выбрать несколько “интересных” номеров итераций, например: 1, 2, 3, 5, 8, 13, 21, 34 …
 
-# Create a mesh of points in the feature space
+fig, axs = plt.subplots(2, 4, figsize=(30, 20))
+axs, ax_index = axs.flatten(), 0
+
 values_amount = 1000
 x_min, x_max = get_bounds(X['x'])
 y_min, y_max = get_bounds(X['y'])
@@ -57,34 +58,23 @@ xs = np.linspace(x_min, x_max, values_amount)
 ys = np.linspace(y_min, y_max, values_amount)
 xx, yy = np.meshgrid(xs, ys)
 
-values = pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=features)
+data = pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=features)
 
-# Predict the class of each point in the mesh
-Zs = list(ada_clf.staged_predict(values))
+Zs = list(ada_clf.staged_predict(data.values))
 
 iter_numbers = [1, 2, 3, 5, 8, 13, 21, 34]
-# Iterate over the number of trees and generate a prediction for each point in the mesh
 for i, score in enumerate(Zs):
-
-    # Select a few "interesting" iteration numbers to visualize
     if i in iter_numbers:
-        # Reshape the prediction to match the shape
         Z = Zs[i].reshape(xx.shape)
 
         cmap = plt.cm.Paired
 
-        # Create a new figure and plot the prediction
         axs[ax_index].pcolormesh(xx, yy, Z, cmap=cmap, alpha=0.5)
-
-        # Overlay the training points on the plot
         axs[ax_index].scatter(X_train['x'], X_train['y'], c=y_train, cmap=cmap)
-
-        # Set the plot limits and labels
         axs[ax_index].set_xlabel('x')
         axs[ax_index].set_ylabel('y')
         axs[ax_index].set_title(f'AdaBoost Classification: iteration {i}')
 
         ax_index += 1
 
-# Show the plot
 plt.show()
--- data-analysis/lab-6/random_forest.py
+++ data-analysis/lab-6/random_forest.py
@@ -9,46 +9,40 @@ def get_bounds(m_values):
     return m_values.min() - 0.5, m_values.max() + 0.5
 
 
-# Load the chips dataset
+# 1. Постройте график зависимости качества классификации от числа деревьев
+# на тестовой и тренировочной части набора данных.
+
 df = pd.read_csv("chips.csv")
 
 features = ['x', 'y']
 
-# Split the data into X (features) and y (target)
 X = df[features]
 y = df['class']
 
-# Split the data into a training set and a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-# Create a list of the number of trees to evaluate
-tree_list = [1, 2, 3, 5, 8, 13, 21, 34]
+tree_amounts = [1, 2, 3, 5, 8, 13, 21, 34]
 
-# Create a dictionary to store the training and test scores
 scores = {'train_score': [], 'test_score': []}
 
-# Iterate through the list of trees and fit a random forest model
-for num_trees in tree_list:
-    model = RandomForestClassifier(n_estimators=num_trees, random_state=42)
-
+for tree_amount in tree_amounts:
+    model = RandomForestClassifier(n_estimators=tree_amount, random_state=42)
     model.fit(X_train, y_train)
 
-    # Get the training and test scores for the model
     scores['train_score'].append(model.score(X_train, y_train))
     scores['test_score'].append(model.score(X_test, y_test))
 
-# Plot the classification quality versus the number of trees on the test and training parts of
-# the dataset
-plt.plot(tree_list, scores['train_score'], label='Train')
-plt.plot(tree_list, scores['test_score'], label='Test')
-plt.xlabel('Trees Number')
+plt.plot(tree_amounts, scores['train_score'], label='Train')
+plt.plot(tree_amounts, scores['test_score'], label='Test')
+plt.xlabel('Trees amount')
 plt.ylabel('Classification Quality')
 plt.legend()
 plt.show()
 
-# Visualize how the entire space is classified by the algorithm for a different number of trees
+# 2. Визуализируйте, как всё пространство классифицируется алгоритмом для разного числа деревьев.
+# Должно получиться несколько картинок. Необязательно строить картинки для каждого шага.
+# Можно выбрать несколько “интересных” номеров итераций, например: 1, 2, 3, 5, 8, 13, 21, 34 …
 
-# Create a figure with subplots
 fig, axs = plt.subplots(2, 4, figsize=(30, 20))
 axs = axs.flatten()
 
@@ -59,26 +53,21 @@ xs = np.linspace(x_min, x_max, values_amount)
 ys = np.linspace(y_min, y_max, values_amount)
 xx, yy = np.meshgrid(xs, ys)
 
-# Wrap ravel_ in a DataFrame with the correct column names
 values = pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=features)
 
-# Iterate through the iteration numbers and plot the classification of the entire space
-for ax_index, num_trees in enumerate(tree_list):
-    model = RandomForestClassifier(n_estimators=num_trees, random_state=42)
+for ax_index, tree_amount in enumerate(tree_amounts):
+    model = RandomForestClassifier(n_estimators=tree_amount, random_state=42)
     model.fit(X, y)
 
-    # Get the predictions for each point in the meshgrid
     Z = model.predict(values)
     Z = Z.reshape(xx.shape)
 
-    cmap = plt.cm.coolwarm
+    cmap = plt.cm.Paired
 
-    # Plot the meshgrid and the points from the training set
-    axs[ax_index].pcolormesh(xx, yy, Z, cmap=cmap, alpha=0.8)
+    axs[ax_index].pcolormesh(xx, yy, Z, cmap=cmap, alpha=0.5)
     axs[ax_index].scatter(X_train['x'], X_train['y'], c=y_train, cmap=cmap)
     axs[ax_index].set_xlabel('x')
     axs[ax_index].set_ylabel('y')
-    axs[ax_index].set_title(f'Random forest: {num_trees} trees')
+    axs[ax_index].set_title(f'Random forest: {tree_amount} trees')
 
-# Show the plot
 plt.show()
